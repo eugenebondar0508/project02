e86f711661eed6541d98b3a3af8d2290ca79817c
eugenebondar0508 <larisa-bondar1@ukr.net>
2021-06-18 21:58:45 +0300

    added first addidional task

--- script/script.js
+++ script/script.js
@@ -131,4 +131,30 @@ window.addEventListener('DOMContentLoaded', function(){
 
     togglePopup();
 
+
+    // anchors
+
+    const serviceBlock = document.querySelector('a[href*="service-block"]');
+    const portfolio = document.querySelector('a[href*="portfolio"]');
+    const calc = document.querySelector('a[href*="calc"]');
+    const command = document.querySelector('a[href*="command"]');
+    const connect = document.querySelector('a[href*="connect"]');
+    
+
+    const anchors = [serviceBlock, portfolio, calc, command, connect];
+
+    for (let anchor of anchors){
+        anchor.addEventListener('click', (event) => {
+            event.preventDefault();
+            const blockID = anchor.getAttribute('href')
+            document.querySelector('' + blockID).scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+
+            })
+        })
+    }
+    
+    
+
 });
